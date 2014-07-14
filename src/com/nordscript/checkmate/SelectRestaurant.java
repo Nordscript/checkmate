026e8c644767334747ba0849e58de21fffce6570
Heiki Riisikamp <heiki.riisikamp@nordscript.com>
2014-07-14 17:18:28 +0200

    First push

---
+++ src/com/nordscript/checkmate/SelectRestaurant.java
@@ -0,0 +1,58 @@
+package com.nordscript.checkmate;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SelectRestaurant extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_restaurant);
+		
+		Button btnLoad = (Button) findViewById(R.id.select);
+		 
+        OnClickListener listener = new OnClickListener() {
+ 
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SelectTable hello = new SelectTable();
+                fragmentTransaction.add(R.id.selectTable, hello, "HELLO");
+                fragmentTransaction.commit();
+           }
+        };
+ 
+        btnLoad.setOnClickListener(listener);
+ 
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.select_restaurant, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ src/com/nordscript/checkmate/SelectTable.java
@@ -0,0 +1,16 @@
+package com.nordscript.checkmate;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class SelectTable extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Inflating the layout for this fragment **/
+        View v = inflater.inflate(R.layout.activity_select_table, null);
+        return v;
+    }
+}
