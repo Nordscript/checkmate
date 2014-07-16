7f2b216ef0a55df8039d8442ef20b4e4da1da931
William Ku <williamku@berkeley.edu>
2014-07-16 22:29:32 +0200

    Testing still

--- src/com/nordscript/checkmate/PaymentPicks.java
+++ src/com/nordscript/checkmate/PaymentPicks.java
@@ -1,43 +1,99 @@
 package com.nordscript.checkmate;
 
 
-import android.app.ListActivity;
+import java.util.ArrayList;
+import java.util.Map;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
 
 
-public class PaymentPicks extends ListActivity {
+public class PaymentPicks extends ActionBarActivity {
+
+	// Declare the UI components
+		private ListView songsListView;
+
+		// Declare an ArrayAdapter that we use to join the data set and the ListView
+		// is the way of type safe, means you only can pass Strings to this array
+		//Anyway ArrayAdapter supports only TextView
+		private ArrayAdapter arrayAdapter;
+
+		private Firebase ref;
+		private PaymentPicks activ;
+		private ArrayList<String> dishes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	activ = this;
+    	dishes = new ArrayList<String>(20);
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_picks);
-		
-
-        setTitle("Food selection");
-
         
-    
-    };
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.select_restaurant, menu);
-		return true;
-	}
+     // Initialize the UI components
+        songsListView = (ListView)findViewById(R.id.listView1);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
+        // Create a reference to a Firebase location
+        ref = new Firebase("https://uqtez5y2bki.firebaseio-demo.com/Restaurants/Restaurant 2/Tables/Table 2/Orders");
 
+        // Read data and react to changes
+        ref.addChildEventListener(new ChildEventListener() {
+        	  @Override
+        	  public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+        	    Map s = snapshot.getValue(Map.class);
+        	    dishes.add(snapshot.getName() + "  " + ((Map)snapshot.getValue(Map.class)).get("Price"));
+        	    Log.i("Test", dishes.toString());
+        	    arrayAdapter = new ArrayAdapter(activ, android.R.layout.simple_list_item_1, dishes.toArray());
+
+                // By using setAdapter method, you plugged the ListView with adapter
+                songsListView.setAdapter(arrayAdapter);
+        	  }
+
+        	  @Override public void onChildChanged(DataSnapshot snapshot, String previousChildName) { }
+
+        	  @Override public void onChildRemoved(DataSnapshot snapshot) {
+        		Map s = snapshot.getValue(Map.class);
+          	    dishes.remove(snapshot.getName() + "  " + ((Map)snapshot.getValue(Map.class)).get("Price"));
+          	    Log.i("Test", dishes.toString());
+          	    arrayAdapter = new ArrayAdapter(activ, android.R.layout.simple_list_item_1, dishes.toArray());
+
+                // By using setAdapter method, you plugged the ListView with adapter
+                songsListView.setAdapter(arrayAdapter);
+        	  }
+
+        	  @Override public void onChildMoved(DataSnapshot snapshot, String previousChildName) { }
+
+        	  @Override public void onCancelled() { }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.payment_picks, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
