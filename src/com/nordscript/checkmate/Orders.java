87822f7c55f75141632bee46dea77c649bc2e7ad
Heiki Riisikamp <heiki.riisikamp@nordscript.com>
2014-07-16 22:05:14 +0200

    Before Firebase

--- src/com/nordscript/checkmate/Orders.java
+++ src/com/nordscript/checkmate/Orders.java
@@ -2,33 +2,54 @@ package com.nordscript.checkmate;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Orders extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_orders);
-	}
+	// Declare the UI components
+		private ListView songsListView;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.orders, menu);
-		return true;
-	}
+		// Declare an array to store data to fill the list
+		private String[] songsArray ;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		// Declare an ArrayAdapter that we use to join the data set and the ListView
+		// is the way of type safe, means you only can pass Strings to this array
+		//Anyway ArrayAdapter supports only TextView
+		private ArrayAdapter arrayAdapter;
+
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_orders);
+
+	        // Initialize the UI components
+	        songsListView = (ListView) findViewById(R.id.songsListView);
+
+	        // Initialize the songs array
+	        songsArray = new String[10];
+
+	        // Fill the songs array by using a for loop
+	        for(int i=0; i < songsArray.length; i++){
+	        	songsArray[i] = "Song " + i;
+	        }
+
+	        // For this moment, you have list of songs and a ListView where you can display a list.
+	        //But how can we put this data set to the list?
+	        //This is where you need an Adapter
+
+	        //context -  The current context. 
+	        //resource - The resource ID for a layout file containing a layout to use when instantiating views. 
+	        //textViewResourceId - The id of the TextView within the layout resource to be populated  
+	        //From the third parameter, you plugged the data set to adapter
+	        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, songsArray);
+
+	        // By using setAdapter method, you plugged the ListView with adapter
+	        songsListView.setAdapter(arrayAdapter);
+
+	        // Normally the argument of setAdapter ask for a ListAdapter instance.
+	        //And that is the best way of implementation of this code
+	        //We call it "programming to the interface"
+	    }
 	}
-}
