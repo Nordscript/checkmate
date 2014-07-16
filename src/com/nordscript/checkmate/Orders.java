47f03ea460996e530eac5a36dbdcb457063771bf
Heiki Riisikamp <heiki.riisikamp@nordscript.com>
2014-07-16 22:22:42 +0200

    Database connected!

--- src/com/nordscript/checkmate/Orders.java
+++ src/com/nordscript/checkmate/Orders.java
@@ -1,9 +1,20 @@
 package com.nordscript.checkmate;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class Orders extends Activity {
 
@@ -17,39 +28,73 @@ public class Orders extends Activity {
 		// is the way of type safe, means you only can pass Strings to this array
 		//Anyway ArrayAdapter supports only TextView
 		private ArrayAdapter arrayAdapter;
+		private Firebase ref;
+		private Orders activ;
+		private ArrayList<String> dishes;
 
 	    /** Called when the activity is first created. */
 	    @Override
 	    public void onCreate(Bundle savedInstanceState) {
+	    	activ = this;
+	    	dishes = new ArrayList<String>(20);
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_orders);
 
 	        // Initialize the UI components
 	        songsListView = (ListView) findViewById(R.id.songsListView);
 
-	        // Initialize the songs array
-	        songsArray = new String[10];
+	        // Create a reference to a Firebase location
+	        ref = new Firebase("https://uqtez5y2bki.firebaseio-demo.com/Restaurants/Restaurant 2/Tables/Table 2/Orders");
 
-	        // Fill the songs array by using a for loop
-	        for(int i=0; i < songsArray.length; i++){
-	        	songsArray[i] = "Song " + i;
+	        // Read data and react to changes
+	        ref.addChildEventListener(new ChildEventListener() {
+	        	  @Override
+	        	  public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+	        	    Map s = snapshot.getValue(Map.class);
+	        	    dishes.add(snapshot.getName() + "  " + ((Map)snapshot.getValue(Map.class)).get("Price"));
+	        	    Log.i("Test", dishes.toString());
+	        	    arrayAdapter = new ArrayAdapter(activ, android.R.layout.simple_list_item_1, dishes.toArray());
+
+	                // By using setAdapter method, you plugged the ListView with adapter
+	                songsListView.setAdapter(arrayAdapter);
+	        	  }
+
+	        	  @Override public void onChildChanged(DataSnapshot snapshot, String previousChildName) { }
+
+	        	  @Override public void onChildRemoved(DataSnapshot snapshot) {
+	        		Map s = snapshot.getValue(Map.class);
+	          	    dishes.remove(snapshot.getName() + "  " + ((Map)snapshot.getValue(Map.class)).get("Price"));
+	          	    Log.i("Test", dishes.toString());
+	          	    arrayAdapter = new ArrayAdapter(activ, android.R.layout.simple_list_item_1, dishes.toArray());
+
+	                // By using setAdapter method, you plugged the ListView with adapter
+	                songsListView.setAdapter(arrayAdapter);
+	        	  }
+
+	        	  @Override public void onChildMoved(DataSnapshot snapshot, String previousChildName) { }
+
+	        	  @Override public void onCancelled(FirebaseError error) { }
+	        });
+
+	    }
+
+
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	        getMenuInflater().inflate(R.menu.orders, menu);
+	        return true;
+	    }
+
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        // Handle action bar item clicks here. The action bar will
+	        // automatically handle clicks on the Home/Up button, so long
+	        // as you specify a parent activity in AndroidManifest.xml.
+	        int id = item.getItemId();
+	        if (id == R.id.action_settings) {
+	            return true;
 	        }
-
-	        // For this moment, you have list of songs and a ListView where you can display a list.
-	        //But how can we put this data set to the list?
-	        //This is where you need an Adapter
-
-	        //context -  The current context. 
-	        //resource - The resource ID for a layout file containing a layout to use when instantiating views. 
-	        //textViewResourceId - The id of the TextView within the layout resource to be populated  
-	        //From the third parameter, you plugged the data set to adapter
-	        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, songsArray);
-
-	        // By using setAdapter method, you plugged the ListView with adapter
-	        songsListView.setAdapter(arrayAdapter);
-
-	        // Normally the argument of setAdapter ask for a ListAdapter instance.
-	        //And that is the best way of implementation of this code
-	        //We call it "programming to the interface"
+	        return super.onOptionsItemSelected(item);
 	    }
 	}
