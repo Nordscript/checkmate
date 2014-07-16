f22ce1f9e68e44ce92e4263a6b5e9f1b48f4dcdc
Heiki Riisikamp <heiki.riisikamp@nordscript.com>
2014-07-16 17:39:16 +0200

    Tried again

--- src/com/nordscript/checkmate/PaymentPicks.java
+++ src/com/nordscript/checkmate/PaymentPicks.java
@@ -1,7 +1,11 @@
 package com.nordscript.checkmate;
 
+
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class PaymentPicks extends ListActivity {
 
@@ -9,10 +13,31 @@ public class PaymentPicks extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_picks);
+		
 
         setTitle("Food selection");
+
         
     
     };
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.select_restaurant, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+
 }
